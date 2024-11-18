1c2629ad8551d95750956650df0a1ff8ba948d96
aftab yunus mulani <aftab.mulani1515@gmail.com>
2024-11-18 23:52:25 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import SignIn from './components/sign-in';
+
 export default function Home() {
-  return <div className="bg-red-400 absolute bottom-0">hollow world</div>;
+  return (
+    <div className="bg-red-400 absolute bottom-0">
+      <SignIn />
+    </div>
+  );
 }
